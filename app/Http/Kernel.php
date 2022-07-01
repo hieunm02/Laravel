cae38aee732643875fb7de85e61f54ae2da8f8d0
hieunm02 <nguyenminhhieu2002ptpt@gmail.com>
2022-07-01 21:27:24 +0700

    Check login user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminloginMiddleware::class,
+        'user_login' => \App\Http\Middleware\CheckLoginUser::class,
+
 
     ];
 }
